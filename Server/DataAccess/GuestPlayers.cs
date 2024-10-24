e302601c422b9ce6a62f7323514b160363f1b734
Fernando Martinez Ramirez <zS22013690@estudiantes.uv.mx>
2024-10-23 20:50:58 -0600

    Actualizacion de la base de datos y arreglo de conflictos

--- Server/DataAccess/GuestPlayers.cs
+++ Server/DataAccess/GuestPlayers.cs
@@ -23,9 +23,7 @@ namespace DataAccess
         public int GuestID { get; set; }
         public string Username { get; set; }
         public Nullable<System.DateTime> JoinDate { get; set; }
-        public int StatusID { get; set; }
     
-        public virtual StatusPlayer StatusPlayer { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<LobbyPlayers> LobbyPlayers { get; set; }
     }
