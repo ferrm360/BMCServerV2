7e89cdd57d28dd64caf82da4ece2107624cb4dc4
Fernando Martinez Ramirez <zS22013690@estudiantes.uv.mx>
2024-10-29 00:58:17 -0600

    Ajuste en mensajes de error y resultados, se actualizan las constantes y las clases de respuesta

---
+++ Server/Service/Utilities/Results/LobbyResponse.cs
@@ -0,0 +1,33 @@
+﻿using Service.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Utilities.Results
+{
+    [DataContract]
+    public class LobbyResponse
+    {
+        [DataMember]
+        public bool IsSuccess { get; set; }
+
+        [DataMember]
+        public string ErrorKey { get; set; }
+
+        [DataMember]
+        public LobbyDTO Lobby { get; set; }
+
+        public static LobbyResponse SuccessResult(LobbyDTO lobby)
+        {
+            return new LobbyResponse { IsSuccess = true, Lobby = lobby };
+        }
+
+        public static LobbyResponse Failure(string errorKey)
+        {
+            return new LobbyResponse { IsSuccess = false, ErrorKey = errorKey };
+        }
+    }
+}
