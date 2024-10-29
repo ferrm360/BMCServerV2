b60d424106ec935662c2fa83432950493310d3a8
Fernando Martinez Ramirez <zS22013690@estudiantes.uv.mx>
2024-10-29 03:12:09 -0600

    Implementacion completa del correo y creacion de un .config encargado de la configuracion

---
+++ Server/Service/Email/EmailConfigHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Net.Mail;
+using System.Net;
+
+namespace Service.Email
+{
+    public static class EmailConfigHelper
+    {
+        public static string GetFromAddress()
+        {
+            return ConfigurationManager.AppSettings["EmailFromAddress"];
+        }
+
+        public static SmtpClient GetSmtpClient()
+        {
+            var smtpClient = new SmtpClient
+            {
+                Host = ConfigurationManager.AppSettings["SmtpHost"],
+                Port = int.Parse(ConfigurationManager.AppSettings["SmtpPort"]),
+                EnableSsl = bool.Parse(ConfigurationManager.AppSettings["EnableSsl"]),
+                Credentials = new NetworkCredential(
+                    ConfigurationManager.AppSettings["SmtpUsername"],
+                    ConfigurationManager.AppSettings["SmtpPassword"]
+                )
+            };
+            return smtpClient;
+        }
+    }
+}
---
+++ Server/Service/Email/EmailServiceFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Service.Email
+{
+    public static class EmailServiceFactory
+    {
+        public static IEmailService CreateEmailService()
+        {
+            var smtpClient = EmailConfigHelper.GetSmtpClient();
+            var fromAddress = EmailConfigHelper.GetFromAddress();
+
+            return new EmailService(smtpClient, fromAddress);
+        }
+    }
+}
