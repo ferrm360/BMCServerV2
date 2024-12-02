7a98f27afc4944fff859c86c2ca60d5abcfccce4
Fernando Martinez Ramirez <zS22013690@estudiantes.uv.mx>
2024-12-01 19:41:59 -0600

    Implementacion de servicio para enviar correos, con la plantilla para cambiar contrasena

---
+++ Server/Service/Contracts/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using Service.DTO;
+using Service.Results;
+using System.ServiceModel;
+
+
+namespace Service.Contracts
+{
+    [ServiceContract]
+    public interface IEmailService
+    {
+        [OperationContract]
+        OperationResponse SendEmail(EmailDTO emailDTO);
+    }
+}
---
+++ Server/Service/Email/Templates/LobbyTemplate.cs
@@ -0,0 +1,41 @@
+﻿using Service.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Email.Templates
+{
+    public static class LobbyTemplate
+    {
+        public static string GetSubject()
+        {
+            return "Invite to lobby";
+        }
+
+        public static string GetBody(EmailDTO emailDTO) {
+            var lobbyDetails = $@"
+                Hi {emailDTO.Username},
+
+                You have been invited to join a game lobby by {emailDTO.LobbyHost}.
+                
+                Lobby Name: {emailDTO.LobbyName}";
+
+            if (!string.IsNullOrWhiteSpace(emailDTO.LobbyPassword))
+            {
+                lobbyDetails += $@"
+                Password: {emailDTO.LobbyPassword}";
+            }
+
+            lobbyDetails += @"
+
+                Enjoy the game!
+
+                Regards,
+                BMC Service Team";
+
+            return lobbyDetails;
+        }
+    }
+}
